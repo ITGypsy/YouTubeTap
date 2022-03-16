9ef16148505537b0b9d0023c4ebb2f2f02c22916
ITGypsy <stash4trash@gmail.com>
2022-03-16 10:42:26 +0300

    Initial Commit

---
+++ YouTubeTap/YouTubeTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeTapApp.swift
+//  YouTubeTap
+//
+//  Created by Kutarba Arthur on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
